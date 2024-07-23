a5d4d202216cf6b929cf929aeb74e721ac9f5d65
zt64 <31907977+zt64@users.noreply.github.com>
2024-07-23 12:57:46 -0400

    chore(deps): remove unused nokee plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
 
-    // Native code compilation
-    alias(libs.plugins.nokee.jni) apply false
-    alias(libs.plugins.nokee.cpp) apply false
-
     // Maintenance
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.kover) apply false
