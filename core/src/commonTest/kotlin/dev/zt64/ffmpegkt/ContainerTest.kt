0bef431329361b4327288b5c57ae3b435f50e13b
zt64 <31907977+zt64@users.noreply.github.com>
2025-06-29 00:35:58 -0400

    test: update ContainerTest to encode frame to an H264 stream

--- core/src/commonTest/kotlin/dev/zt64/ffmpegkt/ContainerTest.kt
+++ core/src/commonTest/kotlin/dev/zt64/ffmpegkt/ContainerTest.kt
@@ -2,13 +2,14 @@ package dev.zt64.ffmpegkt
 
 import dev.zt64.ffmpegkt.codec.Codec
 import dev.zt64.ffmpegkt.codec.CodecID
-import dev.zt64.ffmpegkt.codec.Packet
+import dev.zt64.ffmpegkt.codec.VideoEncoder
 import dev.zt64.ffmpegkt.container.Container
 import dev.zt64.ffmpegkt.stream.VideoStream
 import dev.zt64.ffmpegkt.test.TestResources
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class ContainerTest {
     @Test
@@ -59,15 +60,79 @@ class ContainerTest {
         val path = "./build/test-output/streams-test.mp4"
 
         Container.openOutput(path).use { output ->
-            output.newStream<VideoStream>(Codec.findEncoder(CodecID.MPEG4)!!)
-            output.writeHeader()
+            // TODO: Simplify the stream/encoder creation process
+            val videoStream = output.newStream<VideoStream>(Codec.findEncoder(CodecID.H264)!!)
 
-            // Seems to be necessary for streams to write at all
-            output.mux(Packet(byteArrayOf(4)))
+            val frameRate = 25 // 25 frames per second
+            val durationSeconds = 10
+            val frames = frameRate * durationSeconds
+            val width = 256
+            val height = 256
+
+            val c = VideoEncoder(
+                codec = CodecID.H264,
+                bitrate = 900000,
+                width = width,
+                height = height,
+                framerate = frameRate
+            )
+            c.open()
+
+            videoStream.timeBase = c.timeBase
+            output.writeHeader()
+            val frame = c.createFrame()
+
+            for (i in 0 until frames) {
+                val frameData = frame.data
+
+                val (linesize0, linesize1, linesize2) = frame.linesize
+
+                // Y
+                for (y in 0 until c.height) {
+                    for (x in 0 until c.width) {
+                        frameData[0][y * linesize0 + x] = (x + y + i * 3).toUByte()
+                    }
+                }
+
+                // Cb and Cr
+                for (y in 0 until c.height / 2) {
+                    for (x in 0 until c.width / 2) {
+                        frameData[1][y * linesize1 + x] = (128 + y + i * 2).toUByte()
+                        frameData[2][y * linesize2 + x] = (64 + x + i * 5).toUByte()
+                    }
+                }
+                frame.pts = i.toLong()
+
+                println("Send frame ${frame.pts}")
+
+                c.encode(frame).forEach { packet ->
+                    packet.use {
+                        packet.rescaleTimestamp(c.timeBase, videoStream.timeBase)
+
+                        println("Write packet (size=${packet.size})")
+                        output.mux(packet)
+                    }
+                }
+            }
+
+            frame.close()
+
+            c.encode(null).forEach { packet ->
+                packet.use {
+                    packet.rescaleTimestamp(c.timeBase, videoStream.timeBase)
+                    output.mux(packet)
+                }
+            }
+            c.close()
         }
 
         Container.openInput(path).use { input ->
             assertEquals(1, input.streams.size, "Expected 1 stream in the container")
+            val videoStream = input.streams.video.firstOrNull()
+                ?: fail("video stream should be null at this point")
+            assertEquals(CodecID.H264, videoStream.codecParameters.codecId, "Expected H.264 codec")
+            assertEquals(256, videoStream.codecParameters.width, "Expected video width to be 256")
+            assertEquals(256, videoStream.codecParameters.height, "Expected video height to be 256")
         }
     }
 }
