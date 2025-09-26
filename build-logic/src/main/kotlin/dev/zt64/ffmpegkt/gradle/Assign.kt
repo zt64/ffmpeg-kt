ce532e4fa97e4a5e1028d44980a1809d157e4e16
zt64 <31907977+zt64@users.noreply.github.com>
2025-09-25 22:33:40 -0400

    fix: restrict Property assign function to non-nullable types

--- build-logic/src/main/kotlin/dev/zt64/ffmpegkt/gradle/Assign.kt
+++ build-logic/src/main/kotlin/dev/zt64/ffmpegkt/gradle/Assign.kt
@@ -2,4 +2,4 @@ package dev.zt64.ffmpegkt.gradle
 
 import org.gradle.api.provider.Property
 
-fun <T> Property<T>.assign(value: T) = set(value)
+fun <T : Any> Property<T>.assign(value: T) = set(value)
