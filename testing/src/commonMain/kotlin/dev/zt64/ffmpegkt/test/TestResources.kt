ee64fbd2320e53848dd7236a64b067a2dda030c6
zt64 <31907977+zt64@users.noreply.github.com>
2025-09-25 23:16:49 -0400

    refactor: update test resource path to use Big Buck Bunny video

--- testing/src/commonMain/kotlin/dev/zt64/ffmpegkt/test/TestResources.kt
+++ testing/src/commonMain/kotlin/dev/zt64/ffmpegkt/test/TestResources.kt
@@ -5,7 +5,7 @@ import com.goncalossilva.resources.Resource
 object TestResources {
     private const val BASE_PATH = "../testing/src/commonMain/resources"
 
-    val RESOURCE_1 = Resource("$BASE_PATH/video/SampleVideo_1280x720_5mb.mp4")
+    val RESOURCE_1 = Resource("$BASE_PATH/video/Big_Buck_Bunny_1080_10s_5MB.mp4")
     val MPEG_1_VIDEO = Resource("$BASE_PATH/video/sample_640x360.mpeg")
     val WAV_AUDIO = Resource("$BASE_PATH/audio/Dreaming-of-you_fma-145627_001_00-00-01.mp2")
 }
