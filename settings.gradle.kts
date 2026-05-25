42e6b20d44fc31e894e4c7023e9ae37ef0aac974
zt64 <31907977+zt64@users.noreply.github.com>
2026-05-24 20:21:49 -0400

    chore(deps): update Gradle wrapper to version 9.5.1 and add toolchains plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,10 @@ pluginManagement {
     includeBuild("build-logic")
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
